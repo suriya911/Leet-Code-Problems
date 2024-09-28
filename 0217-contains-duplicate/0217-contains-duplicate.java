8bf742a112796161c7eb7548e63df953d4b53792
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-09-28 12:10:40 -0700

    Time: 22 ms (6.68%), Space: 58.6 MB (21.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> h = new HashMap<>();
-        for(int i: nums){
-            if(h.containsKey(i)){
-                return true;
-            }
-            h.put(i,1);
-        }
-        return false;
+       Set<Integer> s = IntStream.of(nums).boxed().collect(Collectors.toSet());
+        return s.size() != nums.length;
     }
 }
