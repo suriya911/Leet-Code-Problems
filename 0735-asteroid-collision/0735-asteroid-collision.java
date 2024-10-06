718dd53bc645b071dd3525b457701ed07c5ff0e4
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-06 12:08:07 -0700

    Time: 2 ms (97.65%), Space: 45.1 MB (74.38%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        Stack <Integer> stack = new Stack<>();
+    public int[] asteroidCollision(int[] a) {
+        int n = a.length;
+        int s = 0;
+        int[] ans = new int[n];
 
-        for(int a : asteroids){
-            if(a>0){
-                stack.push(a);
-            }
+        for(int j=0;j<n;j++){
+            int cur=a[j];
+            if(cur>0) ans[s++]=cur;
+            
             else{
-                while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -a){
-                    stack.pop();
+                while(s>0 && ans[s-1]>0){
+                    if(ans[s-1] < -cur) s--;
+                    else if(ans[s-1]==-cur){
+                        s--;
+                        cur=0;
+                        break;
+                    }
+                    else{
+                        cur=0;
+                        break;
+                    }
                 }
-
-                if(stack.isEmpty() || stack.peek() < 0){
-                    stack.push(a);
-                }
-
-                if(stack.peek() == -a){
-                    stack.pop();
+                if(cur!=0){
+                    ans[s++]=cur;
                 }
             }
         }
-
-        int[] res = new int[stack.size()];
-        int i=stack.size()-1;
-
-        while(!stack.isEmpty()){
-            res[i--]=stack.pop();
-        }
-
-        return res;
+        return Arrays.copyOf(ans,s);
     }
 }
