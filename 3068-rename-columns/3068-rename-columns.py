b3047f5109efd7fafe749ea5dac2c00c12508cc5
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-06 12:34:15 -0700

    Time: 359 ms (64.26%), Space: 69.5 MB (13.16%) - LeetHub

--- 3068-rename-columns/3068-rename-columns.py
+++ 3068-rename-columns/3068-rename-columns.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
-    students.rename(columns={'id':'student_id','first':'first_name','last':'last_name','age':'age_in_years'},inplace=True)
-
+    students.columns = ["student_id","first_name","last_name","age_in_years"]
     return students
