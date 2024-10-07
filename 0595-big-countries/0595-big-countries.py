678dcbf18ba4d663cc5687031ad50e2107bde05a
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-07 11:13:27 -0700

    Time: 381 ms (41.05%), Space: 71.5 MB (66.43%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    return world[(world['area'] >= 3_000_000) | (world['population'] >= 25_000_000)][['name','population','area']]
