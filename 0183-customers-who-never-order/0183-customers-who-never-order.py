365e613df69bbac7c68381e202074968a1e8646e
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-07 12:31:35 -0700

    Time: 398 ms (41.73%), Space: 70 MB (65.29%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    df = customers[~customers['id'].isin(orders['customerId'])]
-
-    df = df[['name']].rename(columns={'name':'Customers'})
-
-    return df 
+    # df = customers[~customers['id'].isin(orders['customerId'])]
+    # df = df[['name']].rename(columns={'name':'Customers'})
+    return customers[~customers['id'].isin(orders['customerId'])].rename(columns = {'name':'Customers'})[['Customers']]
