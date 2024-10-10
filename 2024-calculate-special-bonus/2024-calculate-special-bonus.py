bdd13d793ad0f992463baccc91e7c5bc925f9d7e
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-10 08:52:57 -0700

    Time: 413 ms (57.76%), Space: 70.7 MB (28.82%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    return employees.assign(
-        bonus=employees.apply(lambda x: x['salary'] if int(x['employee_id']) % 2 != 0 and not x['name'].startswith('M') else 0, axis=1)
-    )[['employee_id', 'bonus']].sort_values(
-        by='employee_id',
-    )
+    employees['bonus'] = np.where((employees['employee_id'] % 2 == 1) & (employees['name'].str[0] != 'M'), employees['salary'], 0)
+    return employees[['employee_id', 'bonus']].sort_values(by = 'employee_id')
