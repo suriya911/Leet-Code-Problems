5e8d661c09dcddecfc0e978a81765db86df5e4e6
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-13 20:18:37 -0700

    Time: 0 ms (100%), Space: 40.2 MB (53.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        if (n<=3) return n;
+
+        int[] arr = new int[n + 1];
+        arr[0] = arr[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
 
-        return dp[n];
+        return arr[n];
     }
 }
