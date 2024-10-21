4a5fd320e606955ac471ad237be9908fdf7b4866
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-10-21 14:15:40 -0700

    Time: 0 ms (100%), Space: 40.8 MB (39.15%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        int cnt = Integer.bitCount(n);
-        return cnt==1;
+       return  (n>0 && (n & (n-1))==0) ? true:false;
     }
 }
