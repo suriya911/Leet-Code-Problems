7f48e713b63338f75a0309244f36486937fdee4c
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-02 14:48:30 -0700

    Time: 1 ms (100%), Space: 42.5 MB (95.69%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,20 +1,29 @@
 class Solution {
-  public String predictPartyVictory(String senate) {
-    var queue = new ArrayDeque<Character>();
+    public String predictPartyVictory(String senate) {
 
-    for (var c : senate.toCharArray())
-      queue.offer(c);
+        char[] c = senate.toCharArray();
+        return solve(c,0);
 
-    var c = 'R';
-    while (!queue.isEmpty()) {
-      c = queue.poll();
-      queue.offer(c);
-
-      if (c == 'R' && !queue.remove('D'))
-        return "Radiant";
-      if (c == 'D' && !queue.remove('R'))
-        return "Dire";
     }
-    return null;
-  }
+
+    public String solve(char[] c , int vote){
+
+        int r = 0 , d = 0;
+
+        for(int i=0;i<c.length;i++){
+            if(c[i] == 'R'){
+                if(vote >= 0) r++;
+                else c[i] = '0';
+                vote++;
+            }else if(c[i] == 'D'){
+                if(vote <= 0) d++;
+                else c[i] = '0';
+                vote--;
+            }
+        }
+
+        if(r != 0 && d == 0) return "Radiant";
+        if(d != 0 && r == 0) return "Dire";
+        return solve(c,vote);
+    }
 }
