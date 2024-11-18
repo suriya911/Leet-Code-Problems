78969a505a269ab0d271de1f68ff6cbea04b8e77
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-18 07:48:57 -0800

    Time: 518 ms (32.79%), Space: 92.7 MB (81.18%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.py
+++ 2057-count-salary-categories/2057-count-salary-categories.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
 def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame({
-        'category': ['Low Salary','Average Salary', 'High Salary'],
-        'accounts_count': [
-            accounts[accounts.income<20000].shape[0],
-            accounts[(accounts.income>=20000) & (accounts.income<=50000)].shape[0],
-            accounts[accounts.income>50000].shape[0]
-        ],
-    })
+    df = accounts
+    low = len(df[df['income'] < 20000])
+    avrg = len(df[(df['income'] >= 20000) & (df['income'] <= 50000)])
+    high = len(df[df['income'] > 50000])
+    df_new = pd.DataFrame({'category': ['Low Salary', 'Average Salary', 'High Salary'], 'accounts_count': [low, avrg, high]})
+    return df_new
