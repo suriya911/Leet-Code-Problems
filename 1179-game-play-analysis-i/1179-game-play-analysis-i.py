b4a1b74d3b4a2ecc9a1d00c54106487a6861c1ee
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-19 20:08:26 -0800

    Time: 375 ms (75.72%), Space: 71 MB (35.91%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.py
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    first_login = activity.groupby('player_id')['event_date'].agg(min).reset_index()
-
-    first_login = first_login.rename(columns={'event_date':'first_login'})
-
+    first_login = activity.groupby('player_id')['event_date'].min().reset_index()
+    
+    first_login.columns = ['player_id', 'first_login']
+    
     return first_login
