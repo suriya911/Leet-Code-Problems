69a3703fb215483373495a124bb77c4ee6c21ba1
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-22 13:44:34 -0800

    Time: 47 ms (86.59%), Space: 50 MB (82.36%) - LeetHub

--- 2805-array-wrapper/2805-array-wrapper.js
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -17,7 +17,8 @@ ArrayWrapper.prototype.valueOf = function() {
  * @return {string}
  */
 ArrayWrapper.prototype.toString = function() {
-    return `[${this.nums.join(',')}]`;
+    // return `[${this.nums.join(',')}]`;
+    return  JSON.stringify(this.nums)
 }
 
 /**
