6fb19ed0a24dc2dfb11478fb689b7beeedd756b9
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-23 12:56:37 -0800

    Time: 0 ms (100%), Space: 7.7 MB (91.47%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int r{0};
+
+        for(int i=5;i<=n;i*=5){
+            r = r + (n/i);
+        }
+
+        return r; 
+
+
+    }
+};
