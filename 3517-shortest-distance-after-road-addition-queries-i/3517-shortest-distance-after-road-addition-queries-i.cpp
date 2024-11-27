3b3a60b1228b831ea929fdabe9f7b54de7c05010
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-27 12:19:40 -0800

    Time: 12 ms (98.36%), Space: 52.1 MB (97.42%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,37 +1,29 @@
 class Solution {
+    void dfs(vector<vector<int>>& tree, int c, vector<int>& dp) {
+        int d=dp[c]+1;
+        for (int x : tree[c]) {
+            if (dp[x]<=d) continue;
+            dp[x]=d;
+            dfs(tree,x,dp);
+        }
+    }
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n);
-        for (int i = 0; i < n; i++) {
-            adj[i].push_back(i + 1);
-        }
-        
-        auto shortest_path = [&]() {
-            queue<pair<int, int>> q;
-            q.push({0, 0}); // node, length
-            unordered_set<int> visit;
-            visit.insert(0);
-            
-            while (!q.empty()) {
-                auto [cur, length] = q.front();
-                q.pop();
-                
-                if (cur == n - 1) return length;
-                
-                for (int nei : adj[cur]) {
-                    if (visit.find(nei) == visit.end()) {
-                        q.push({nei, length + 1});
-                        visit.insert(nei);
-                    }
-                }
-            }
-            return -1;
-        };
-        
-        vector<int> res;
-        for (const auto& query : queries) {
-            adj[query[0]].push_back(query[1]);
-            res.push_back(shortest_path());
+        vector<int> dp(n);
+        for (int i=0; i<n; ++i) dp[i]=n-1-i;
+        vector<vector<int>> tree(n);
+        for (int i=0; i+1<n; ++i)
+            tree[i+1].push_back(i);
+        int m=int(queries.size());
+        vector<int> res(m);
+        int i=0;
+        for (auto& q : queries) {
+            int a=q[0], b=q[1];
+            tree[b].push_back(a);
+            dp[a]=min(dp[a],dp[b]+1);
+            dfs(tree,a,dp);
+            res[i]=dp[0];
+            ++i;
         }
         return res;
     }
