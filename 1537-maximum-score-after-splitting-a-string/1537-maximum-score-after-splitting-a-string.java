c547769c8be021733e2af5ebea6973a2db81bcdd
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-01-01 15:43:04 -0800

    Time: 0 ms (100%), Space: 41.4 MB (64.46%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,26 +1,23 @@
-public class Solution {
-    public int maxScore(String inputStr) {
-        int totalOnes = 0;
-        int zerosCount = 0;
-        int onesCount = 0;
-        int bestScore = Integer.MIN_VALUE;
+class Solution {
+    public int maxScore(String s) {
+        int n = s.length();
+        int ons = 0;
+        int curr = s.charAt(0) == '0' ? 1 : 0;
+        int score = curr;
 
-        // Count total ones in the string
-        for (char ch : inputStr.toCharArray()) {
-            if (ch == '1') totalOnes++;
+        for (int i = 1; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                curr++;
+            } else {
+                ons++;
+                curr--;
+            }
+            if (curr > score) {
+                score = curr;
+            }
         }
+        ons += s.charAt(n - 1) == '1' ? 1 : 0;
 
-        // Traverse the string and calculate scores
-        for (int i = 0; i < inputStr.length() - 1; i++) {
-            if (inputStr.charAt(i) == '0') zerosCount++;
-            else onesCount++;
-
-            // Calculate score
-            int currentScore = zerosCount + (totalOnes - onesCount);
-            bestScore = Math.max(bestScore, currentScore);
-
-        }
-
-        return bestScore;
+        return ons + score;
     }
 }
