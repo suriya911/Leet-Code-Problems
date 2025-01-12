20528e8bfdace3224a019b839746e3b1c4d7bd6d
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-01-11 18:40:21 -0800

    Time: 15 ms (51.52%), Space: 30 MB (79.7%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    static bool canBeValid(string& s, string& locked) {
-        const int n=s.size();
-        if (n&1) return 0;
-        int bMin=0, bMax=0;
-        for(int i=0; i<n; i++){
-            bool op=s[i]=='(', wild=locked[i]=='0';
-            bMin+=(!op|wild)?-1:1;
-            bMax+=(op|wild)?1:-1;
-            if (bMax<0) return 0;
-            bMin=max(bMin, 0);
+    bool canBeValid(string s, string locked) {
+        if(s.length()%2) return false;
+        int min = 0, max = 0;
+        for(int i=0; i<s.length(); i++){
+            if(locked[i]=='1'){
+                if(s[i]=='('){
+                    min++;
+                    max++;
+                }else{
+                    min--;
+                    max--;
+                }
+            }else{
+                min--;
+                max++;
+            }
+            if(min<0) min=0;
+            if(max<0) return false;
         }
-        return bMin==0;
+        return (min==0);
     }
 };
-
-
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
