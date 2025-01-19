aacf98c4082a32168b9f200a0697c419e36a0727
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-01-18 23:39:29 -0800

    Time: 79 ms (96.84%), Space: 33 MB (5.64%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -1,40 +1,21 @@
+import numpy as np
+
 class Solution:
-    def trapRainWater(self, height: List[List[int]]) -> int:
-        dir = (0, 1, 0, -1, 0)
-        m, n = len(height), len(height[0])
-        if m <= 2 or n <= 2:
-            return 0
 
-        boundary = []
-        for i in range(m):
-            boundary.append((height[i][0], i, 0))
-            boundary.append((height[i][-1], i, n - 1))
-            height[i][0] = height[i][-1] = -1
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
 
-        for j in range(1, n - 1):
-            boundary.append((height[0][j], 0, j))
-            boundary.append((height[-1][j], m - 1, j))
-            height[0][j] = height[-1][j] = -1
+        # Fill the water in
+        water_level = np.array(heightMap, dtype=int)
+        heightMap = np.array(heightMap, dtype=int)
+        water_level[1:-1, 1:-1] = np.max(heightMap)
 
-        heapify(boundary)
-        ans, water_level = 0, 0
-
-        while boundary:
-            h, i, j = heappop(boundary)
-
-            water_level = max(water_level, h)
-
-            for a in range(4):
-                i0, j0 = i + dir[a], j + dir[a + 1]
-                if i0 < 0 or i0 >= m or j0 < 0 or j0 >= n or height[i0][j0] == -1:
-                    continue
-                currH = height[i0][j0]
-                if currH < water_level:
-                    ans += water_level - currH
-
-                height[i0][j0] = -1
-                heappush(boundary, (currH, i0, j0))
-        return ans
- 
-
-        
+        # Let the water flow out
+        w_old = 0
+        while True:
+            wl_pad = np.pad(water_level, ((1, 1), (1, 1)))
+            water_level = np.maximum(np.minimum(np.minimum(wl_pad[0:-2, 1:-1], wl_pad[2:, 1:-1]),
+                                                np.minimum(wl_pad[1:-1, 0:-2], wl_pad[1:-1, 2:])), heightMap)
+            w = np.sum(water_level - heightMap)
+            if w_old == w:
+                return int(w)
+            w_old = w
