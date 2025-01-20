dcd0651d8f7f71087abce0f0a8448e781bf9b5f0
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-01-20 00:40:49 -0800

    Time: 11 ms (96.87%), Space: 18.1 MB (6.05%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,19 +1,28 @@
-class Solution(object):
-    def threeSumClosest(self, nums, target):
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        closest_sum = float('inf')
+        n = len(nums)
+        ans = sum(nums[:3])
+        if len(nums) <= 3:
+            return ans
+        for l in range(n-2):
+            # prune search
+            low,high = nums[l]+nums[l+1]+nums[l+2],nums[l]+nums[-2]+nums[-1]
+            ans = min(ans,low,high,key=lambda v:abs(v-target))
+            if target < low or target > high:
+                continue
+            # 2pointer
+            m,r = l+1,n-1
+            while m<r:
+                summ = nums[l]+nums[m]+nums[r]
+                if abs(target-summ) < abs(target-ans):
+                    ans = summ
+                if summ < target:
+                    m+=1
+                elif summ > target:
+                    r-=1
+                else: # exact solution
+                    return ans
+        return ans
+            
         
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-                if abs(current_sum - target) < abs(closest_sum - target):
-                    closest_sum = current_sum
-                if current_sum < target:
-                    left += 1
-                elif current_sum > target:
-                    right -= 1
-                else:
-                    return current_sum
-        
-        return closest_sum
