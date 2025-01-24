fc73a887a5c277960e850dd49f06924f87b38936
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-01-23 18:59:25 -0800

    Time: 618 ms (17.74%), Space: 67.5 MB (70.36%) - LeetHub

---
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    return (
+        teacher
+        .groupby('teacher_id', as_index=False)['subject_id']
+        .nunique()
+        .rename(columns={'subject_id':'cnt'})
+    )
