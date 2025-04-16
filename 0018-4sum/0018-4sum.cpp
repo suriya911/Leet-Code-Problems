c33e84c789464a9de8128695966d4fe7400c3e21
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-04-15 19:07:43 -0700

    Time: 35 ms (20.81%), Space: 17.5 MB (59.95%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,34 +1,26 @@
-//Optimized Approach using two pointer - O(n^3) time and O(n) space
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            for(int j = i+1; j < nums.size(); j++){
-                int k = j + 1;
-                int l = nums.size() - 1;
-                while (k < l) {
-                    //by writing below 4 statement this way it will not give runtime error
-                    long long int sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if (sum == target) {
-                        s.insert({nums[i], nums[j], nums[k], nums[l]});
-                        k++;
-                        l--;
-                    } else if (sum < target) {
-                        k++;
-                    } else {
-                        l--;
+        int n = nums.size();
+        for(int i=0; i<n-3; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j = i+1; j<n-2; j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    if(nums[i] + 0LL + nums[j] + 0LL + nums[k] + 0LL == target - nums[l] + 0LL){
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while(k<l && nums[k] == nums[k+1]) k++;
+                        while(k<l && nums[l] == nums[l-1]) l--;
                     }
+                    if(nums[i] + 0LL + nums[j] + 0LL + nums[k] + 0LL < target - nums[l] + 0LL) k++;
+                    else l--;
                 }
             }
         }
-        for(auto quadruplets : s)
-            output.push_back(quadruplets);
-        return output;
+        return ans;
     }
 };
