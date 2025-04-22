14acf81d1bea0b1224ffe6186bb4edaa734837bc
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-04-21 18:30:19 -0700

    Time: 47 ms (85.71%), Space: 18.3 MB (92.06%) - LeetHub

--- 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.py
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.py
@@ -1,20 +1,16 @@
-from collections import Counter
-from math import comb
-
 class Solution:
-    def idealArrays(self, length: int, max_value: int) -> int:
-        MOD = 1_000_000_007
-        total_ways = max_value
-        frequency_map = {num: 1 for num in range(1, max_value + 1)}
-        
-        for array_size in range(1, length): 
-            new_frequency = Counter()
-            for base_value in frequency_map: 
-                for multiplier in range(2, max_value // base_value + 1): 
-                    combinations = comb(length - 1, array_size)
-                    total_ways += combinations * frequency_map[base_value]
-                    new_frequency[multiplier * base_value] += frequency_map[base_value]
-            frequency_map = new_frequency
-            total_ways %= MOD
-        
-        return total_ways
+    def idealArrays(self, n: int, maxValue: int) -> int:
+        @lru_cache(None)
+        def countWithout1s(n:int,maxVal:int): # num of distinct numbers 
+            if n == 1:
+                return maxVal - 1
+            ans=0
+            for i in range(2, maxVal // 2**(n-1) + 1):
+                ans += countWithout1s(n-1, maxVal//i)
+            return ans
+
+        MOD, comb, ans = 10**9+7, 1, 1
+        for i in range(1,floor(log2(maxValue))+1):
+            comb = comb * (n - i + 1) // i
+            ans += comb * countWithout1s(i, maxValue)
+        return ans%MOD
