8d7643a91c70dac903d97acfad68d02a44b63e24
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-06-06 13:06:14 +0530

    Time: 215 ms (93.89%), Space: 20.8 MB (49.62%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,29 +1,13 @@
-class Solution(object):
-    def robotWithString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        from collections import Counter
-
-        freq = Counter(s)
-        st = []
-        res = []
-        
-        def min_char(freq):
-            for i in range(26):
-                ch = chr(ord('a') + i)
-                if freq[ch] > 0:
-                    return ch
-            return 'a'
-
-        for ch in s:
-            st.append(ch)
-            freq[ch] -= 1
-            while st and st[-1] <= min_char(freq):
-                res.append(st.pop())
-
-        while st:
-            res.append(st.pop())
-
-        return ''.join(res)
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        s = collections.deque(s)
+        t = []
+        paper = []
+        for c in sorted(set(s)):
+            while t and t[-1] <= c:
+                paper.append(t.pop())  # t -> paper
+            while c in s:
+                while s[0] != c:
+                    t.append(s.popleft())  # s -> t
+                paper.append(s.popleft())  # s -> t -> paper
+        return "".join(paper)
