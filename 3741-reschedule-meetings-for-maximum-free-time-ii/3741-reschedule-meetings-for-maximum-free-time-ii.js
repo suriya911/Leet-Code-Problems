3f0de29645e73df07c3c500d1dc98adec8d9d116
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-07-10 19:48:39 +0530

    Time: 16 ms (42.86%), Space: 76 MB (28.57%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.js
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.js
@@ -4,39 +4,35 @@
  * @param {number[]} endTime
  * @return {number}
  */
-const maxFreeTime = (eventTime, startTime, endTime) => {
-    const len = startTime.length;
-    const gaps = new Array(len + 1);
-    let lastEnd = 0;
+var maxFreeTime = function (eventTime, startTime, endTime) {
+    const n = startTime.length;
+    const q = new Array(n).fill(false);
+    let t1 = 0,
+        t2 = 0;
+    for (let i = 0; i < n; i++) {
+        if (endTime[i] - startTime[i] <= t1) {
+            q[i] = true;
+        }
+        t1 = Math.max(t1, startTime[i] - (i === 0 ? 0 : endTime[i - 1]));
 
-    startTime.forEach((s, i) => {
-        gaps[i] = s - lastEnd;
-        lastEnd = endTime[i];
-    });
+        if (endTime[n - i - 1] - startTime[n - i - 1] <= t2) {
+            q[n - i - 1] = true;
+        }
+        t2 = Math.max(
+            t2,
+            (i === 0 ? eventTime : startTime[n - i]) - endTime[n - i - 1],
+        );
+    }
 
-    gaps[len] = eventTime - lastEnd;
-
-    const rightMax = new Array(len + 1).fill(0);
-    rightMax.reduceRight((a, val, i) => {
-        if (i < len) rightMax[i] = Math.max(rightMax[i + 1], gaps[i + 1]);
-        return a;
-    }, 0);
-
-    let leftMax = 0;
-    let maxGap = 0;
-
-    startTime.map((s, i) => i + 1).forEach(i => {
-        const dur = endTime[i - 1] - startTime[i - 1];
-        const gapL = gaps[i - 1];
-        const gapR = gaps[i];
-
-        if (leftMax >= dur || rightMax[i] >= dur)
-            maxGap = Math.max(maxGap, gapL + dur + gapR);
-
-        maxGap = Math.max(maxGap, gapL + gapR);
-        leftMax = Math.max(leftMax, gapL);
-    });
-
-    return maxGap;
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        const left = i === 0 ? 0 : endTime[i - 1];
+        const right = i === n - 1 ? eventTime : startTime[i + 1];
+        if (q[i]) {
+            res = Math.max(res, right - left);
+        } else {
+            res = Math.max(res, right - left - (endTime[i] - startTime[i]));
+        }
+    }
+    return res;
 };
-
