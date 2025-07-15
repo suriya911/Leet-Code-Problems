647e612d0cb94a96168af18e0316d047c9eddc98
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-07-15 22:28:23 +0530

    Time: 1 ms (99.12%), Space: 42.1 MB (62.25%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    private static final String VOWELS = "aeiouAEIOU";
+
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        int v = 0, c = 0;
+        for (char ch : word.toCharArray()) {
+            if (Character.isDigit(ch)) continue;
+
+            if (Character.isLetter(ch)) {
+                if (VOWELS.indexOf(ch) >= 0) ++v;
+                else ++c;
+            } else {
+                return false;
+            }
+        }
+        return v > 0 && c > 0;
+    }
+}
