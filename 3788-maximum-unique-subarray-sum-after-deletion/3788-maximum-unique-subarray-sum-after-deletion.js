1efe593983ca132879bbdce53972167038481a73
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-07-25 12:45:21 +0530

    Time: 1 ms (80%), Space: 55.5 MB (68%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSum = nums => {
+    if (nums.every(c => c < 0)) return Math.max(...nums);
+    const unique = new Set(nums);
+
+    return [...unique].filter(c => c > 0).reduce((a, c) => a + c, 0)
+}
