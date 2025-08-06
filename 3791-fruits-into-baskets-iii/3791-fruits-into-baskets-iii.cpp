1f4716594fdab012d182eb258379ee276f832c88
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-08-05 17:04:44 -0700

    Time: 0 ms (100%), Space: 189.3 MB (34.82%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -1,97 +1,78 @@
+auto init=atexit([](){ofstream("display_runtime.txt")<<"0";});
+class Segment_Tree {
+    vector<int> seg;
+public:
+    Segment_Tree(int n){
+        seg.resize(4*n+1);
+    }
+
+    void build(int idx,int low,int high,vector<int>& v) {
+        if(low==high) {
+            seg[idx] = v[low];
+            return;
+        }
+        int mid = (low+high)/2;
+        build(2*idx+1,low,mid,v);
+        build(2*idx+2,mid+1,high,v);
+        seg[idx] = max(seg[2*idx+1],seg[2*idx+2]);
+    }
+
+    void update(int idx,int low,int high,int i,int val) {
+        if(low==high) {
+            seg[idx] = val;
+            return;
+        }
+        int mid = (low+high)/2;
+        if(i<=mid)
+            update(2*idx+1,low,mid,i,val);
+        else
+            update(2*idx+2,mid+1,high,i,val);
+        seg[idx] = max(seg[2*idx+1],seg[2*idx+2]);
+    }
+
+    int query(int idx,int low,int high,int l,int r) {
+
+        if(low>=l && high<=r)   return seg[idx];
+
+        if(high<l || low>r)     return INT_MIN;
+
+        int mid = (low+high)/2;
+        int left = query(2*idx+1,low,mid,l,r);
+        int right = query(2*idx+2,mid+1,high,l,r);
+        return max(left,right);
+    }
+};
+
 class Solution {
 public:
-    int numOfUnplacedFruits(std::vector<int>& fruits, std::vector<int>& baskets) {
-        int result = fruits.size();
-        std::vector<int> tree = createTree(baskets.size());
+    int numOfUnplacedFruits(vector<int>& f, vector<int>& b) {
+        int n = b.size();
+        Segment_Tree st(n);
+        st.build(0,0,n-1,b);
 
-        addArray(tree, baskets);
-        calculateTree(tree);
+        int ans = n;
+        for(int a:f) {
 
-        for (int fruit : fruits) {
-            int pos = findLeftmostPos(tree, fruit);
-            if (pos != -1) {
-                result--;
-                removePos(tree, pos);
+            int sub = -1;
+            int l=0,r=n-1;
+            while(l<=r) {
+                int mid = (l+r)/2;
+                int cap = st.query(0,0,n-1,0,mid);
+                if(cap >= a) {
+                    sub = mid;
+                    r = mid-1;
+                }
+                else {
+                    l = mid+1;
+                }
+            }
+
+            if(sub != -1) {
+                --ans;
+                st.update(0,0,n-1,sub,0);
             }
         }
 
-        return result;
-    }
-
-private:
-    std::vector<int> createTree(int itemsCount) {
-        if (itemsCount == 0) return std::vector<int>();
-
-        int treeSize = 1;
-        while (treeSize <= itemsCount / 2) {
-            treeSize <<= 1;
-        }
-        if (treeSize < itemsCount) {
-            treeSize <<= 1;
-        }
-        treeSize = (treeSize << 1) - 1;
-
-        return std::vector<int>(treeSize, 0);
-    }
-
-    void addArray(std::vector<int>& tree, const std::vector<int>& arr) {
-        int pos = tree.size() / 2;
-        for (int item : arr) {
-            tree[pos++] = item;
-        }
-    }
-
-    void calculateTree(std::vector<int>& tree) {
-        int n = tree.size();
-        for (int i = (n / 2) - 1; i >= 0; i--) {
-            int left = tree[(i << 1) + 1];
-            int right = tree[(i << 1) + 2];
-            tree[i] = std::max(left, right);
-        }
-    }
-
-    int findLeftmostPos(const std::vector<int>& tree, int val) {
-        if (tree[0] < val) {
-            return -1;
-        }
-
-        int mid = tree.size() / 2;
-        int pos = 0;
-
-        while (pos < mid) {
-            int leftIdx = (pos << 1) + 1;
-            int rightIdx = (pos << 1) + 2;
-            if (tree[leftIdx] >= val) {
-                pos = leftIdx;
-            } else {
-                pos = rightIdx;
-            }
-        }
-
-        return pos;
-    }
-
-    void removePos(std::vector<int>& tree, int pos) {
-        tree[pos] = 0;
-
-        while (pos > 0) {
-            int parent;
-            if (pos & 1) {
-                parent = pos >> 1;
-            } else {
-                parent = (pos >> 1) - 1;
-            }
-
-            int left = tree[(parent << 1) + 1];
-            int right = tree[(parent << 1) + 2];
-            int maxVal = std::max(left, right);
-
-            if (tree[parent] != maxVal) {
-                tree[parent] = maxVal;
-                pos = parent;
-            } else {
-                break;
-            }
-        }
+        return ans;
     }
 };
