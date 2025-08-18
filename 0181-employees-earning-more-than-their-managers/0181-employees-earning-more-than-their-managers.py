c228997819e04cf6f867ca3a3be06e6f953abb5c
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-08-18 16:30:41 -0700

    Time: 346 ms (15.48%), Space: 68.3 MB (79.65%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    df = employee.merge(right = employee, how = 'inner', left_on = 'managerId', right_on = 'id')
+    df = employee.merge(right = employee, how = 'left', left_on = 'managerId', right_on = 'id')
     emp = df[df['salary_x'] > df['salary_y']]['name_x']
     return pd.DataFrame({'Employee':emp})
