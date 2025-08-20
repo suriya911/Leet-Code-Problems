8a2ebf3eda099ad66985efa36aea23d193e1f517
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-08-20 14:48:47 -0700

    Time: 0 ms (100%), Space: 29.5 MB (68.06%) - LeetHub

---
+++ 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int n = nums.size();
+        int sm = INT_MAX;
+
+        for (int win = l; win <= r; win++) {  // window size iterate karo
+            int s = 0;
+
+            // pehla window sum
+            for (int i = 0; i < win; i++) s += nums[i];
+            if (s > 0) sm = min(sm, s);
+
+            // slide window
+            for (int i = win; i < n; i++) {
+                s += nums[i];
+                s -= nums[i - win];
+                if (s > 0) sm = min(sm, s);
+            }
+        }
+
+        return (sm == INT_MAX ? -1 : sm);
+    }
+};
