c9d49e7a261b6355cb7e1970b40858e64fdf7070
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-10-15 17:18:11 -0700

    Time: 10 ms (65.44%), Space: 120.6 MB (77.91%) - LeetHub

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        vector<int> rests(value, 0);
+        int mini = nums.size();
+        for (int num : nums) {
+            if (num >= 0 || num % value == 0)
+                rests[num % value]++;
+            else
+                rests[value - (abs(num) % value)]++;
+        }
+
+        for (int i = 0; i < value; i++) {
+            mini = min(mini, rests[i] * value + i);
+        }
+
+        return mini;
+    }
+};
