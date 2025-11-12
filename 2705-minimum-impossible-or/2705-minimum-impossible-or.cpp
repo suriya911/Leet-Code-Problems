64d7a975c7b0960090bdc3ff6c08ae205c99ac46
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-11-11 20:28:21 -0800

    Time: 23 ms (67.3%), Space: 51.4 MB (94.79%) - LeetHub

---
+++ 2705-minimum-impossible-or/2705-minimum-impossible-or.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minImpossibleOR(vector<int>& nums) {
+        long long int ans=1;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ans){
+                ans*=2;
+            }
+        }
+        return ans;
+    }
+};
