4c395580b149c59363aded47692ac4df6372050a
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-11-16 23:01:54 -0800

    Time: 1 ms (72.19%), Space: 17.6 MB (98.37%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,7 +1,26 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def helper(left,right):
-            if left == right:
-                return nums[left]
-            return max(nums[left] - helper(left+1,right),nums[right]-helper(left,right-1))
-        return helper(0,len(nums)-1) >=0
+        
+        n = len(nums)
+
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = nums[i]
+
+        for length in range(2, n + 1):
+            for l in range(n - length + 1):
+
+                r = length + l - 1
+
+                takeLeft = nums[l] - dp[l + 1][r] 
+
+                takeRight = nums[r] - dp[l][r - 1]
+
+                dp[l][r] = max(takeLeft, takeRight)
+        
+        return dp[0][n - 1] >= 0
+
+
+
+
