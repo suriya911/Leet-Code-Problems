a776cd9c312c8a959bd6b36a25ef03ca92e219d7
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-11-27 23:23:41 -0800

    Time: 9 ms (25.7%), Space: 21.1 MB (31.21%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,9 +1,4 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        result = []
-        total_numbers = 1<<n
-        for i in range(total_numbers):
-            print(i,i>>1)
-            result.append(i ^ (i>>1))
-        return result
+        return [(i >> 1) ^ i for i in range(1 << n)]
         
