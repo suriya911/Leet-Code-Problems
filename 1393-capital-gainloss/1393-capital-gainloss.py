68b0f6f2b927975aaa7f57857a3a5f66dfb1e6d4
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-12-04 20:54:59 +0530

    Time: 280 ms (95%), Space: 68.1 MB (68.82%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.py
+++ 1393-capital-gainloss/1393-capital-gainloss.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from numpy import where
 
 def capital_gainloss(stocks: pd.DataFrame) -> pd.DataFrame:
-    stocks.price *= where(stocks.operation=='Buy',-1,1)
-    return (stocks.groupby('stock_name')['price'].sum().reset_index(name = 'capital_gain_loss'))
+    stocks.loc[stocks['operation']=='Buy', 'price'] = -stocks['price']
+    stocks = stocks.groupby('stock_name')['price'].sum().reset_index(name='capital_gain_loss')
+    return stocks
