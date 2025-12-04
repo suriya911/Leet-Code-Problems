c48dd9d990d2193691cc3c3873dc27038c97695c
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-12-04 20:47:55 +0530

    Time: 441 ms (93.78%), Space: 0B (100%) - LeetHub

---
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select stock_name,sum(
+    case
+        when operation = 'Buy' Then -price
+        else price
+    end
+)as capital_gain_loss
+from stocks
+group by stock_name
