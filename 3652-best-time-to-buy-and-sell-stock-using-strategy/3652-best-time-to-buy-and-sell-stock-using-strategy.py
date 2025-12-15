936c760cec5bad1daf7c4c6d0b9155ea3e7e4930
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2025-12-15 21:56:35 +0530

    Time: 190 ms (98.6%), Space: 31.2 MB (93.42%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
+        n = len(prices)
+        h = k//2
+        sp =[s*p for s,p in zip(strategy, prices)]
+        base = sum(sp)
+
+        if n==k:
+            change = sum(prices[h:]) - base
+            return base + max(0,change)
+
+        win_orign = sum(sp[:k])
+        win_mod = sum(prices[h:k])
+        max_ch = win_mod-win_orign
+
+        for i in range(1,n-k+1):
+            win_orign += sp[i+k-1]-sp[i-1]
+            win_mod += prices[i+k-1] - prices[i-1+h]
+            max_ch = max(max_ch,win_mod-win_orign)
+
+        return base + max(0,max_ch)
