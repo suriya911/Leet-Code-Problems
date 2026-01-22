22e0ad380457c001092b73fc2ed4c8e5c097776f
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2026-01-22 23:54:19 +0530

    Time: 7 ms (95.68%), Space: 19.3 MB (18.99%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
@@ -1,19 +1,24 @@
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        def isSorted(nums, n) -> bool:
-            for i in range(1,n):
-                if nums[i] < nums[i - 1]: return False
-            return True
-        ans, n = 0, len(nums)
-        while not isSorted(nums, n):
-            ans += 1
-            min_sum, pos = float('inf'), -1
-            for i in range(1,n):
-                sum = nums[i - 1] + nums[i]
-                if sum < min_sum:
-                    min_sum = sum
-                    pos = i
-            nums[pos - 1] = min_sum
-            for i in range(pos, n-1): nums[i] = nums[i + 1]
-            n -= 1
-        return ans
+        c=0
+        i=0
+        n=len(nums)
+        while i<n-1:
+            if(nums[i]>nums[i+1]):
+                mini=math.inf
+                index=0
+                for j in range(n-1):
+                    if(mini>(nums[j]+nums[j+1])):
+                        mini=nums[j]+nums[j+1]
+                        index=j
+                nums[index]=mini
+                nums.pop(index+1)
+                c=c+1
+                n=n-1
+                i=0
+            else:
+                i=i+1
+        return c
+
+
+        
