541a5e9861c4cbe46b5fc1154fb961a5159f91df
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2026-03-15 19:58:02 -0700

    Time: 275 ms (94.23%), Space: 23.5 MB (8.65%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.py
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.py
@@ -1,28 +1,33 @@
 class Solution:
     def getBiggestThree(self, grid: List[List[int]]) -> List[int]:
-        m, n = len(grid), len(grid[0])
-        max_length = min((m - 1) // 2, (n - 1) // 2)
-        best = []
-        heapify(best)
-        def rhombusSum(r, c, k):
-            result = grid[r][c]
-            for s in range(k):
-                r += 1
-                result += grid[r][c + s + 1]
-                result += grid[r][c - s - 1]
-            for s in range(k - 1, -1, -1):
-                r += 1
-                result += grid[r][c + s]
-                if s > 0:
-                    result += grid[r][c - s]
-            return result
-        for r in range(m):
-            for c in range(n):
-                for k in range(max_length + 1):
-                    if c >= k and c + k < n and r + 2 * k < m:
-                        candidate = rhombusSum(r, c, k)
-                        if candidate not in set(best):
-                            heappush(best, candidate)
-                        if len(best) > 3:
-                            heappop(best)
-        return sorted(best, reverse = True)
+        rows, cols =len(grid), len(grid[0])
+
+        leftDiag = [[x for x in y] for y in grid]
+        rightDiag = [[x for x in y] for y in grid]
+
+        for i in range(rows):
+            for j in range(cols):
+                if i+1<rows and j-1>=0:
+                    leftDiag[i+1][j-1]+=leftDiag[i][j]
+                if i+1<rows and j+1<cols:
+                    rightDiag[i+1][j+1]+=rightDiag[i][j]
+        
+        sums=[]
+        for i in range(rows):
+            for j in range(cols):
+                maxLen=min((rows-i+1)//2,j+1,cols-j)
+                for k in range(maxLen):
+                    if k==0:
+                        sums.append(grid[i][j])
+                    else:
+                        s1=leftDiag[i+k][j-k]-leftDiag[i][j]
+                        s2=leftDiag[i+2*k][j]-leftDiag[i+k][j+k]
+                        s3=rightDiag[i+k][j+k]-rightDiag[i][j]
+                        s4=rightDiag[i+2*k][j]-rightDiag[i+k][j-k]
+                        sums.append(s1+s2+s3+s4-grid[i+k*2][j]+grid[i][j])
+        
+        sums=set(sums)
+        return heapq.nlargest(3,sums)
+                        
+
+                    
